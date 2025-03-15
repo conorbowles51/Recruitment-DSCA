2c705f09c0ec888db4580ab50783995c92c0940c
conorbowles51 <conorbowles2000@gmail.com>
2025-03-15 15:32:38 +0000

    inital commit

---
+++ server/data/candidates.js
@@ -0,0 +1,10 @@
+export const candidates = [
+    {
+        candidateId: 0,
+        name: "John Smith",
+        email: "johnsmith@gmail.com",
+        experience: 4,
+        applicationStatus: "pending",
+        jobTitle: "front-end developer"
+    }
+];
